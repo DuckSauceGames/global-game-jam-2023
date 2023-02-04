0104d64f770810cb9a9cd7b992b0446605647661
pmaitland <33162414+pmaitland@users.noreply.github.com>
2023-02-04 12:39:51 +0000

    Collect veg (#5)
    * can collect veg
    
    * missed some stuff

---
+++ Roots and Froots/Assets/Scripts/Veg.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Veg : MonoBehaviour {
+
+    public enum Vegetable {
+        CARROT,
+        ONION,
+        POTATO
+    }
+
+    public Vegetable type;
+    public float value;
+    
+}
---
+++ Roots and Froots/Assets/Scripts/Worm.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Worm : MonoBehaviour
+{
+
+    private class VegDetails {
+        public Veg.Vegetable type;
+        public int currentCount;
+
+        public VegDetails(Veg.Vegetable type) { this.type = type; }
+    }
+
+    private List<VegDetails> allVegDetails;
+
+    private int maxVegCount = 5;
+
+    public RootGrid rootGrid;
+
+    private SpriteRenderer spi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allVegDetails = new List<VegDetails>();
+        System.Enum.GetValues(typeof(Veg.Vegetable)).Cast<Veg.Vegetable>().ToList().ForEach(v => allVegDetails.Add(new VegDetails(v)));
+
+        spi = GetComponent<SpriteRenderer>();
+    }
+
+    //public Vector2 speed = new Vector2(5,5);
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(5 * inputX, 5 * inputY, 0);
+        //Vector3 movement = new Vector3(inputX, inputY, 0);
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+        //transform.Rotate();
+        if (inputX < 0) {spi.flipX = true;}
+        if (inputX > 0) {spi.flipX = false;}
+        //Debug.Log($"{speed.x} {speed.y}");
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        Veg veg = collider.gameObject.GetComponent<Veg>();
+
+        if (veg != null) {
+            if (GetCurrentVegCount() >= maxVegCount) return;
+
+            rootGrid.CollectVeg(collider.gameObject);
+
+            foreach (VegDetails vegDetails in allVegDetails) {
+                if (vegDetails.type == veg.type) vegDetails.currentCount++;
+                Debug.Log(vegDetails.type.ToString() + " " + vegDetails.currentCount);
+            }
+        } else {
+            Debug.Log("idk what i'm colliding with");
+        }
+    }
+
+    private int GetCurrentVegCount() {
+        int count = 0;
+        foreach (VegDetails vegDetails in allVegDetails) count += vegDetails.currentCount;
+        return count;
+    }
+}
