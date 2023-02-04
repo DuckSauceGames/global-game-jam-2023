9bc43dd31507584a502aef4964c2e22404e8de2d
pmaitland <33162414+pmaitland@users.noreply.github.com>
2023-02-04 22:34:28 +0000

    stuff and things (#23)

--- Roots and Froots/Assets/Scripts/UI/Inventory.cs
+++ Roots and Froots/Assets/Scripts/UI/Inventory.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
+using UnityEngine.UI;
 using System.Linq;
 
 public class Inventory : MonoBehaviour
