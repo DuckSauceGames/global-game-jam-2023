b2ce50388b4ca5b7436cce94bce1ed138825d156
Noel <14931745+FireRedNinja@users.noreply.github.com>
2023-02-04 17:04:39 +0000

    Currency UI Script

---
+++ Roots and Froots/Assets/Scripts/UI/Currency.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Currency : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+
+    private void Start() {
+        transform.GetComponent<TextMeshProUGUI>();
+        SetCurrencyValue(0);
+    }
+    public void SetCurrencyValue(int value) {
+        string curr_name;
+        if (value == 1) {
+            curr_name = "groat";
+        } else {
+            curr_name = "groats";
+        }
+        text.text = value.ToString() + curr_name;
+    }
+}
