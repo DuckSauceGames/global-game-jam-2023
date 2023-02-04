4e0278d6c89d873fc16b60d636b0479a4ae58485
Noel Rajan <14931745+FireRedNinja@users.noreply.github.com>
2023-02-04 16:16:38 +0000

    Stats Bars Scripts (#12)

---
+++ Roots and Froots/Assets/Scripts/UI/StatsBar.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsBar : MonoBehaviour
+{
+    public GameObject stamina;
+    public GameObject health;
+}
