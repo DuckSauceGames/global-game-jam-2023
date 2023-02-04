3c393789de6b4384a1fdc4cafa6d1f5c91d800a4
pmaitland <pmaitland98@gmail.com>
2023-02-04 12:30:39 +0000

    can collect veg

--- Roots and Froots/Assets/Scripts/RootGrid.cs
+++ Roots and Froots/Assets/Scripts/RootGrid.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class RootGrid : MonoBehaviour {
 
-    public int width = 25;
+    public int width = 100;
     public int height = 100;
+
+    private float vegGap = 2f;
 
     [System.Serializable]
     public class VegDetails {
+        public Veg.Vegetable type;
         public GameObject prefab;
         public int maxCount;
         public int respawnChance;
@@ -20,9 +23,7 @@ public class RootGrid : MonoBehaviour {
         public void DecreaseCount() { currentCount--; }
     }
 
-    public VegDetails potatoDetails;
-    public VegDetails carrotDetails;
-    public VegDetails onionDetails;
+    public List<VegDetails> allVegDetails;
 
     private List<List<GameObject>> grid = new List<List<GameObject>>();
     
@@ -31,33 +32,47 @@ public class RootGrid : MonoBehaviour {
         for (int i = 0; i < height; i++) column.Add(null);
         for (int i = 0; i < width; i++) grid.Add(column);
 
-        plantInitialVeg(potatoDetails);
-        plantInitialVeg(carrotDetails);
-        plantInitialVeg(onionDetails);
+        allVegDetails.ForEach(v => PlantInitialVeg(v));
     }
 
     void Update() {
-        respawnVeg(potatoDetails);
-        respawnVeg(carrotDetails);
-        respawnVeg(onionDetails);
+        allVegDetails.ForEach(v => RespawnVeg(v));
     }
 
-    private void plantInitialVeg(VegDetails vegDetails) {
+    private void PlantInitialVeg(VegDetails vegDetails) {
         for (int i = 0; i < vegDetails.maxCount; i++) {
-            plantVeg(vegDetails);
+            PlantVeg(vegDetails);
         }
     }
 
-    private void plantVeg(VegDetails vegDetails) {
+    private void PlantVeg(VegDetails vegDetails) {
         int x = Random.Range(0, width);
         int y = Random.Range(0, height);
         if (grid[x][y] == null) {
-            grid[x][y] = GameObject.Instantiate(vegDetails.prefab, new Vector2(x/10f, y/10f), Quaternion.identity, this.transform);
+            grid[x][y] = GameObject.Instantiate(vegDetails.prefab, new Vector2((x/10f) + (x/10f)*vegGap, (y/10f) + (y/10f)*vegGap), Quaternion.identity, this.transform);
             vegDetails.IncreaseCount();
         }
     }
 
-    private void respawnVeg(VegDetails vegDetails) {
-        if (vegDetails.Count() < vegDetails.maxCount && Random.Range(1, vegDetails.respawnChance) == 1) plantVeg(vegDetails);
+    private void RespawnVeg(VegDetails vegDetails) {
+        if (vegDetails.Count() < vegDetails.maxCount && Random.Range(1, vegDetails.respawnChance) == 1) PlantVeg(vegDetails);
+    }
+
+    public void CollectVeg(GameObject vegObject) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (grid[x][y] == vegObject) {
+                    Veg veg = vegObject.GetComponent<Veg>();
+
+                    foreach (VegDetails vegDetails in allVegDetails) {
+                        if (vegDetails.type == veg.type) vegDetails.DecreaseCount();
+                    }
+
+                    Destroy(vegObject);
+
+                    return;
+                }
+            }
+        }
     }
 }
